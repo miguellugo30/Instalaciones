ca0f4ebde4c446e48aba6d71908ad78c34463ea8
Miguel Lugo <ingmchlugo@gmail.com>
2019-05-19 16:10:01 -0500

    secure asset

--- config/adminlte.php
+++ config/adminlte.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'title' => 'Instalaciones',
+    'title' => 'Instalaciones ALEX',
 
     'title_prefix' => '',
 
@@ -30,9 +30,9 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Instalaciones</b>ALEX',
 
-    'logo_mini' => '<b>A</b>LT',
+    'logo_mini' => '<b>A</b>LE',
 
     /*
     |--------------------------------------------------------------------------
